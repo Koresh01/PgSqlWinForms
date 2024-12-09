5280d29b8a517ac72221da773f47bbcbdb1db7e0
Koresh01 <kmatveev2004@gmail.com>
2024-12-09 22:40:57 +0300

    Реализована кнопка "добавить".

--- Project/Project/Form1.Designer.cs
+++ Project/Project/Form1.Designer.cs
@@ -29,42 +29,227 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            dataGridView1 = new DataGridView();
+            dataGridView = new DataGridView();
             bindingSource1 = new BindingSource(components);
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            addBtn = new Button();
+            textBoxSurname = new TextBox();
+            textBoxName = new TextBox();
+            textBoxOtchestvo = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBoxStreet = new TextBox();
+            textBoxHouse = new TextBox();
+            textBoxKorpys = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBoxApartment = new TextBox();
+            label7 = new Label();
+            textBoxPhone = new TextBox();
+            label8 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
             ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
             SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridView
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 297);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(1040, 372);
-            dataGridView1.TabIndex = 0;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(12, 297);
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowTemplate.Height = 25;
+            dataGridView.Size = new Size(1040, 372);
+            dataGridView.TabIndex = 0;
             // 
-            // bindingSource1
+            // addBtn
             // 
-            bindingSource1.CurrentChanged += bindingSource1_CurrentChanged;
+            addBtn.Location = new Point(669, 242);
+            addBtn.Name = "addBtn";
+            addBtn.Size = new Size(77, 49);
+            addBtn.TabIndex = 1;
+            addBtn.Text = "Добавить";
+            addBtn.UseVisualStyleBackColor = true;
+            addBtn.Click += OnAddBtnClick;
+            // 
+            // textBoxSurname
+            // 
+            textBoxSurname.Location = new Point(76, 18);
+            textBoxSurname.Name = "textBoxSurname";
+            textBoxSurname.Size = new Size(144, 23);
+            textBoxSurname.TabIndex = 2;
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(76, 47);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(144, 23);
+            textBoxName.TabIndex = 3;
+            // 
+            // textBoxOtchestvo
+            // 
+            textBoxOtchestvo.Location = new Point(76, 76);
+            textBoxOtchestvo.Name = "textBoxOtchestvo";
+            textBoxOtchestvo.Size = new Size(144, 23);
+            textBoxOtchestvo.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(25, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 79);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Отчество";
+            // 
+            // textBoxStreet
+            // 
+            textBoxStreet.Location = new Point(357, 18);
+            textBoxStreet.Name = "textBoxStreet";
+            textBoxStreet.Size = new Size(144, 23);
+            textBoxStreet.TabIndex = 8;
+            // 
+            // textBoxHouse
+            // 
+            textBoxHouse.Location = new Point(357, 47);
+            textBoxHouse.Name = "textBoxHouse";
+            textBoxHouse.Size = new Size(144, 23);
+            textBoxHouse.TabIndex = 9;
+            // 
+            // textBoxKorpys
+            // 
+            textBoxKorpys.Location = new Point(357, 76);
+            textBoxKorpys.Name = "textBoxKorpys";
+            textBoxKorpys.Size = new Size(144, 23);
+            textBoxKorpys.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(310, 21);
+            label4.Name = "label4";
+            label4.Size = new Size(41, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Улица";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(320, 50);
+            label5.Name = "label5";
+            label5.Size = new Size(31, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Дом";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(304, 79);
+            label6.Name = "label6";
+            label6.Size = new Size(47, 15);
+            label6.TabIndex = 13;
+            label6.Text = "Корпус";
+            // 
+            // textBoxApartment
+            // 
+            textBoxApartment.Location = new Point(639, 31);
+            textBoxApartment.Name = "textBoxApartment";
+            textBoxApartment.Size = new Size(95, 23);
+            textBoxApartment.TabIndex = 14;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(579, 34);
+            label7.Name = "label7";
+            label7.Size = new Size(54, 15);
+            label7.TabIndex = 15;
+            label7.Text = "Комната";
+            // 
+            // textBoxPhone
+            // 
+            textBoxPhone.Location = new Point(639, 60);
+            textBoxPhone.Name = "textBoxPhone";
+            textBoxPhone.Size = new Size(188, 23);
+            textBoxPhone.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(579, 63);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 15);
+            label8.TabIndex = 17;
+            label8.Text = "Телефон";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1064, 681);
-            Controls.Add(dataGridView1);
+            Controls.Add(label8);
+            Controls.Add(textBoxPhone);
+            Controls.Add(label7);
+            Controls.Add(textBoxApartment);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBoxKorpys);
+            Controls.Add(textBoxHouse);
+            Controls.Add(textBoxStreet);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxOtchestvo);
+            Controls.Add(textBoxName);
+            Controls.Add(textBoxSurname);
+            Controls.Add(addBtn);
+            Controls.Add(dataGridView);
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
             ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private DataGridView dataGridView;
         private BindingSource bindingSource1;
+        private Button addBtn;
+        private TextBox textBoxSurname;
+        private TextBox textBoxName;
+        private TextBox textBoxOtchestvo;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBoxStreet;
+        private TextBox textBoxHouse;
+        private TextBox textBoxKorpys;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBoxApartment;
+        private Label label7;
+        private TextBox textBoxPhone;
+        private Label label8;
     }
 }
--- Project/Project/Form1.cs
+++ Project/Project/Form1.cs
@@ -1,20 +1,97 @@
 using Npgsql;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
 namespace Project
 {
     public partial class Form1 : Form
     {
+        // Строка подключения к базе данных PostgreSQL
+        string connectionString = "Host=localhost;Username=postgres;Password=admin;Database=TelephoneManager";
+
         public Form1()
         {
             InitializeComponent();
+            ShowTable();
         }
 
-        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        /// <summary>
+        /// Загружает данные на экран.
+        /// </summary>
+        private void ShowTable()
         {
-
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM Phonebook";  // SQL запрос для получения всех данных из таблицы
+                    using (var cmd = new NpgsqlCommand(query, conn))
+                    {
+                        using (var adapter = new NpgsqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);  // Заполнение DataTable данными из базы
+                            dataGridView.DataSource = dt;  // Установка DataSource для DataGridView
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при загрузке данных: {ex.Message}");
+                }
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        /// <summary>
+        /// Обработчик нажатия на кнопку "Добавить".
+        /// </summary>
+        private void OnAddBtnClick(object sender, EventArgs e)
         {
+            // Получение значений с формы (например, текстовых полей)
+            string familia = textBoxSurname.Text;
+            string personName = textBoxName.Text;
+            string otchestvo = textBoxOtchestvo.Text;
+
+            string street = textBoxStreet.Text;
+            string house = textBoxHouse.Text;
+            string korpys = textBoxKorpys.Text;
+            string apartment = textBoxApartment.Text;
+            string phone = textBoxPhone.Text;
+
+            // SQL запрос для добавления новой записи в таблицу
+            string query = @"INSERT INTO Phonebook (famillia, person_name, otchestvo, street, house, korpys, apartment, phone)
+                             VALUES (@famillia, @person_name, @otchestvo, @street, @house, @korpys, @apartment, @phone)";
+
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (var cmd = new NpgsqlCommand(query, conn))
+                    {
+                        // Добавление параметров в запрос
+                        cmd.Parameters.AddWithValue("famillia", familia);
+                        cmd.Parameters.AddWithValue("person_name", personName);
+                        cmd.Parameters.AddWithValue("otchestvo", (object)otchestvo ?? DBNull.Value);  // Обработка возможного null
+                        cmd.Parameters.AddWithValue("street", (object)street ?? DBNull.Value);
+                        cmd.Parameters.AddWithValue("house", (object)house ?? DBNull.Value);
+                        cmd.Parameters.AddWithValue("korpys", (object)korpys ?? DBNull.Value);
+                        cmd.Parameters.AddWithValue("apartment", (object)apartment ?? DBNull.Value);
+                        cmd.Parameters.AddWithValue("phone", phone);
+
+                        cmd.ExecuteNonQuery();  // Выполнение запроса
+
+                        MessageBox.Show("Запись успешно добавлена!");
+                        ShowTable();  // Обновление таблицы
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при добавлении записи: {ex.Message}");
+                }
+            }
         }
     }
 }
